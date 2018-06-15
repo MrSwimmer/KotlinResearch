a8830f5d8331a7ab9ba512e2ba73b0c91f775d5b
mrswimmer <mrswimmerlab@gmail.com>
2018-06-16 01:54:25 +0700

    single note layout more

--- app/src/main/java/com/kotlin_research/kotlinresearch/presentation/notes/NotesController.kt
+++ app/src/main/java/com/kotlin_research/kotlinresearch/presentation/notes/NotesController.kt
@@ -1,14 +1,25 @@
 package com.kotlin_research.kotlinresearch.presentation.settings
 
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
+import com.bluelinelabs.conductor.RouterTransaction
 import com.hannesdorfmann.mosby3.mvp.conductor.MvpController
 import com.kotlin_research.kotlinresearch.App
 import com.kotlin_research.kotlinresearch.R
+import com.kotlin_research.kotlinresearch.presentation.single_note.SingleNoteController
 
 class NotesController : MvpController<NotesContract.View, NotesContract.Presenter>(), NotesContract.View {
+
+    @BindView(R.id.notes_recycler)
+    lateinit var recyclerView: RecyclerView
+    @BindView(R.id.notes_fab)
+    lateinit var floatingActionButton: FloatingActionButton
 
     override fun createPresenter(): NotesContract.Presenter {
         return NotesPresenter()
@@ -19,5 +30,10 @@ class NotesController : MvpController<NotesContract.View, NotesContract.Presente
         ButterKnife.bind(this, view)
         App.getComponent().inject(this)
         return view
+    }
+
+    @OnClick(R.id.notes_fab)
+    fun onFABClick() {
+        router.pushController(RouterTransaction.with(SingleNoteController()))
     }
 }
